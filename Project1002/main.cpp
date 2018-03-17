1d2df9799cdfc96c7e1b282f0b7f3fcd2ff6c6d2
rcc <rcc@10.10.105.35>
2018-03-17 10:06:40 -0700

    Add files via upload

--- Project1002/main.cpp
+++ Project1002/main.cpp
@@ -6,7 +6,7 @@
  *       Class: CIS 17A
  *   Professor: Mark Lehr
  *        Days: MW Time: 12:45p - 2:30
- * 	   Purpose: Moving data with structures
+ * 	   Purpose: Moving data plus profits with structures 
  */
 // Include header files
 #include <iostream>
@@ -22,14 +22,16 @@ struct MovieData {
 	char director[LINE];
 	int year;
 	int langth;
+        int cost;
+        int revenue; 
 };
 // Function prototypes
 void DisplayMovieDat(MovieData Movie);
 
 int main(int argc, char** argv) 
 {
-	MovieData Movie1 = {"Diamonds Are Forever","Guy Hamilton", 1971, 120};
-	MovieData Movie2 = {"The Right Stuff","Philip Kaufman", 1983, 192};
+	MovieData Movie1 = {"Diamonds Are Forever","Guy Hamilton", 1971, 120, 7500000, 120000000};
+	MovieData Movie2 = {"The Right Stuff","Philip Kaufman", 1983, 192, 27000000, 21100000 };
 
 	DisplayMovieDat(Movie1);
 	cout << endl;
@@ -43,6 +45,7 @@ void DisplayMovieDat(MovieData Movie)
 	cout << setw(TABOVER) << "Director: " << Movie.director << endl;
 	cout << setw(TABOVER) << "Year Released: " << Movie.year << endl;
 	cout << setw(TABOVER) << "Running Time: " << Movie.langth << " minutes." << endl;
+        cout << setw(TABOVER) << "Profits: " << Movie.revenue - Movie.cost << endl;
 	return;
 }
 
